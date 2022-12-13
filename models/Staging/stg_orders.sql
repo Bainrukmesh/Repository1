4d4109d3a50a8be64d122222dfecb1a0a02f6f9c
sweetgolu153@gmail.com <sweetgolu153@gmail.com>
2022-12-13 15:41:36 +0000

    test

--- models/Staging/stg_orders.sql
+++ models/Staging/stg_orders.sql
@@ -1,3 +1,24 @@
-select *,
+select
+--from raw_orders
+orderid,
+orderdate,
+shipdate,
+shipmode,
+o.customerid,
+o.product id,
+ordersellingprice,
+ordercostprice,
+--from raw_customer
+customername,
+segment,
+country,
+--from raw_product
+category,
+productname,
+subcategory,
 ordersellingprice - ordercostprice as orderprofit
-from raw.globalmart.orders
+from {{ ref('raw_orders') }} as o
+left join {{ ref('raw_customer') }} as c
+on o.customerid = c.customerid
+left join {{ ref('raw_product') }} as p
+on o.productid = p.productid
